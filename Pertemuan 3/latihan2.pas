19da0d2cdb195957df0ec057359263624f778412
ArieArdinataKusuma-251401046 <arieardinatakusuma3@gmail.com>
2025-10-10 23:10:30 +0700

    Update latihan2.pas
    perbaikan dari kesalahan kemarin

--- Pertemuan 3/latihan2.pas
+++ Pertemuan 3/latihan2.pas
@@ -13,18 +13,12 @@ begin
     write('Masukkan batu 3: ');
     readln(c);
 
-    // batu : 100, 40, 60 --> 40, 60, 100
+    // batu : 100, 40, 60 ---> 40, 60, 100
     if (a > b) then
     begin
         temp:= a;
         a:= b;
         b:= temp;
-    end;
-    if (b > c) then
-    begin
-        temp:= b;
-        b:= c;
-        c:= temp;
     end;
     if (a > c) then
     begin
@@ -32,5 +26,11 @@ begin
         a:= c;
         c:= temp;
     end;
-    writeln(a, ' ', b, ' ', c)
+    if (b > c) then
+    begin
+        temp:= b;
+        b:= c;
+        c:= temp;
+    end;
+    writeln(a, ' ', b, ' ', c);
 end.
